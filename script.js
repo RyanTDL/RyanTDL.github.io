d9a8741a480d1ee13caefea661e5df85cdd7dfd3
RyanTDL <teodeliangryan@gmail.com>
2023-08-26 23:33:47 +0800

    First draft of Navbar and Home finished

---
+++ script.js
@@ -0,0 +1,12 @@
+// const text= document.querySelector('.intro');
+// const textLoad= () => {
+//     setTimeout(() => {
+//         text.textContent= 'Welcome to my website!';
+//     }, 0);
+//     setTimeout(() => {
+//         text.textContent= 'This is a test!';
+//     }, 4000);
+//     setTimeout(() => {
+//         text.textContent= 'Hope this works!';
+//     }, 8000);
+// }
